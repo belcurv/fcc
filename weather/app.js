407df4ffb847087c1cb48f3ca266a2e086488e17
Jay Schwane <jrschwane@uwalumni.com>
2016-12-20 22:06:03 -0600

    Weather App code cleanup

--- weather/app.js
+++ weather/app.js
@@ -1,24 +1,41 @@
 (function () {
 
     'use strict';
-    
+
     angular.module('weatherApp', [])
-    
+
         .factory('weatherFac', ['$q', '$http', '$window', function ($q, $http, $window) {
-            
-            
-            /* Get user location w/geolocation
+
+            /* Get user location
              * Async operation requires promise ($q)
              *
-             * @params                  [none]
-             * @returns   [object]      [coordinates obj or error]
+             * @params                [none]
+             * @returns   [object]    [coordinates obj or error]
              */
             function getCurrentPosition() {
                 var deferred = $q.defer();
-                
+
                 if (!$window.navigator.geolocation) {
-                    deferred.reject('Browser does not support geolocation');
+
+                    // fallback to IP based location
+                    $http.get('http://ipinfo.io')
+                        .then(function (response) {
+
+                            // Response lat/long comes as single sting.
+                            // getWeather() expects a serparate lat & long.
+                            // First step: convert to an array
+                            var coords = response.data.loc.split(',');
+
+                            // then 'return' as a lat/long object
+                            deferred.resolve({
+                                latitude : coords[0],
+                                longitude: coords[1]
+                            });
+                        });
+
                 } else {
+                    
+                    // html5 browser geolocation
                     $window.navigator.geolocation.getCurrentPosition(
                         function (pos) {
                             deferred.resolve(pos.coords);
@@ -28,10 +45,10 @@
                         }
                     );
                 }
-                
+
                 return deferred.promise;
             }
-            
+
             
             /* Get local weather
              * $http GET method returns a promise
@@ -40,43 +57,51 @@
              * @returns  [object]            [weather results]
              */
             function getWeather(coords) {
-                var lat     = coords.latitude,
-                    long    = coords.longitude,
-                    baseUrl = "http://forecast.weather.gov/MapClick.php",
-                    fullUrl = baseUrl + "?lat=" + lat + "&lon=" + long + "&FcstType=json";
+                var fullUrl = [
+                    'http://forecast.weather.gov/MapClick.php',
+                    '?lat=', coords.latitude,
+                    '&lon=', coords.longitude,
+                    '&FcstType=json'
+                ].join('');
 
                 return $http.get(fullUrl)
                     .then(
                         function (response) {
-                            console.log('Status: ' + response.status);
-                            console.log('Data', response.data);
+                            // success
                             return response.data.currentobservation;
                         },
                         function (response) {
+                            // error
                             console.log('Status: ' + response.status);
                         }
                     );
             }
-            
-            
+
             // export the methods
             return {
                 getCurrentPosition: getCurrentPosition,
                 getWeather: getWeather
             };
-            
+        
         }])
-    
+
         .controller('weatherController', ['weatherFac', function (weatherFac) {
-            
+
             var vm = this,
                 degSign = String.fromCharCode(parseInt('00B0', 16)),
-                units = 'F';
-            
-            
+                units   = 'F';
+
             // init view object; to be filled by updateView()
             vm.weather = {};
-                        
+
+
+            // random NOAA background image
+            vm.background = [
+                'http://www.photolib.noaa.gov/bigs/libr0',
+                Math.floor((Math.random() * 100) + 300),
+                '.jpg'
+            ].join('');
+
 
             // IIEF to kick it all off
             (function getUserLocation() {
@@ -88,23 +113,25 @@
                         vm.weather.units = degSign + ' ' + units;
                     });
             }());
-            
 
-            // handle "change units" button event clicks
-            // just gets DOM value, manipulates it, and sticks it back into the DOM
+            
+            /* Handle "change units" button
+             *
+             * @params   [number]   temp    [temperature]
+             * @params   [string]   units   [degree symbol & unit letter]
+            */
             vm.changeUnits = function (temp, units) {
-                
-                // units is a string of length = 3, consisting of the
-                // deg symbol + unit letter. We target 3rd position:
+
+                // units.length = 3 : the deg symbol + unit letter
                 if (units[2] === 'F') {
-                    vm.weather.Temp = ((temp - 32) * 5 / 9).toFixed(0);
+                    vm.weather.Temp  = ((temp - 32) * 5 / 9).toFixed(0);
                     vm.weather.units = degSign + ' C';
                 } else {
-                    vm.weather.Temp = ((temp * 9 / 5) + 32).toFixed(0);
+                    vm.weather.Temp  = ((temp * 9 / 5) + 32).toFixed(0);
                     vm.weather.units = degSign + ' F';
                 }
             };
-
+        
         }]);
 
 }());
