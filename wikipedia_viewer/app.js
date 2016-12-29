2e0202a67fb2777368dca2e71ca0e84df16de6b3
Jay Schwane <jrschwane@uwalumni.com>
2016-12-29 14:17:41 -0600

    minor code cleanup

--- wikipedia_viewer/app.js
+++ wikipedia_viewer/app.js
@@ -1,9 +1,3 @@
-/*
-User Story: I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.
-
-User Story: I can click a button to see a random Wikipedia entry.
-*/
-
 /*jslint */
 /*global angular*/
 
