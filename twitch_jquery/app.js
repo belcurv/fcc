f1eaed763266897c4488f287819d0e47886d2af9
Jay Schwane <jrschwane@uwalumni.com>
2017-01-02 03:04:50 -0600

    finish object literal module refacor

--- twitch_jquery/app.js
+++ twitch_jquery/app.js
@@ -4,146 +4,162 @@
 $(document).ready(function () {
     'use strict';
     
-    // Usernames
-    var users = ["ogamingsc2", "ESL_SC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "Reckful", "noobs2ninjas", "brunofin"],
-        route = {
-            users  : 'https://wind-bow.gomix.me/twitch-api/users/',
-            channel: 'https://wind-bow.gomix.me/twitch-api/channels/',
-            streams: 'https://wind-bow.gomix.me/twitch-api/streams/'
-        };
-    
-    // ========================== UTILITY FUNCTIONS ===========================
-
-    function getStream(channel) {
-        // get the channels stream
-        return $.getJSON(route.streams + channel.name)
-            .then(function (st) {
-                // return object consisting of channel + stream
-                return {
-                    channel: channel,
-                    stream: st
-                };
-            });
-    }
-
-    // filter streams
-    function filterObject(data, status) {
+    var twitchModule = {
         
-        if (status === 'all') {
-            // no filtering - return all streams
-            return data;
-        } else if (status === 'Online') {
-            // return only online streams
-            return (data.stream.stream) ? data : false;
-        } else {
-            // return only 'null' streams
-            return (data.stream.stream === null) ? data : false;
-        }
+        state: [],
         
-    }
-
-    function updateDom(data, status) {
+        users: ['ogamingsc2', 'ESL_SC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'brunofin'],
         
-        var outputEl = $('.twitch'),
-            div     = $("<div class='user'></div>"),
-            obj = {},
-            mess;
+        api: {
+            channel : 'https://wind-bow.gomix.me/twitch-api/channels/',
+            stream  : 'https://wind-bow.gomix.me/twitch-api/streams/'
+        },
         
-//        console.log(data ? data : '');
-//        console.log(outputEl);
+        init: function () {
+            this.cacheDom();
+            this.bindEvents();
+            this.users.forEach(this.getChannel.bind(this));
+        },
         
-        if (data && !data.channel.error) {
+        cacheDom: function () {
+            this.$el         = $("#twitchModule");
+            this.$table      = this.$el.find("#table");
+            this.$btnAll     = this.$el.find("#btnAll");
+            this.$btnOnline  = this.$el.find("#btnOnline");
+            this.$btnOffline = this.$el.find("#btnOffline");
+        },
+        
+        bindEvents: function () {
+            this.$btnAll.on("click",     {cond: "all"},     this.filter.bind(this));
+            this.$btnOnline.on("click",  {cond: "online"},  this.filter.bind(this));
+            this.$btnOffline.on("click", {cond: "offline"}, this.filter.bind(this));
+        },
+        
+        getChannel: function (user) {
+            $.getJSON(this.api.channel + user, this.getStream.bind(this));
+        },
+        
+        getStream: function (channel) {
+            $.getJSON(this.api.stream + channel.name)
+                .then(stream => ({ channel: channel, stream: stream }))
+                .then(this.bindData.bind(this))
+                .then(this.loadAll.bind(this));
+        },
+        
+        bindData: function (data) {
+            this.state.push(data);
+        },
+        
+        loadAll: function () {
+            this.$btnAll.trigger('click');
+        },
+        
+        filter: function (event) {
+            var filteredArr = [];
             
-            obj = {
-                name   : data.channel.name,
-                display_name: data.channel.display_name,
-                logo   : data.channel.logo,
-                bio    : data.channel.status ? data.channel.status : '',
-                url    : data.channel.url,
-                status : data.stream.stream ? 'Online' : 'Offline'
-            };
-
-            if (data.stream.stream) {
-                obj.stream = data.stream.stream.game;
+            if (event.data.cond === 'online') {
+                this.render(this.state.filter(c => c.stream.stream ));
+            } else if (event.data.cond === 'offline') {
+                this.render(this.state.filter(c => !c.stream.stream ));
+            } else {
+                this.render(this.state);
             }
+        },
+        
+        render: function (data) {
+            var $table = this.$table,
+                $thead = $table.find('thead'),
+                $tbody = $table.find('tbody');
+            
+            $tbody.empty();
                         
-            div.append('<img src="' + obj.logo + '" class="pic" />')
-                .append('<a href="' + obj.url + '" target="_blank">'
-                        + obj.display_name
-                        + '</a>')
-                .append('<p class="bio">' + obj.bio + '</p>')
-                .append('<p class="status">Status: ' + obj.status + '</p>');
+            $thead
+                .empty()
+                .append('<th>Channel</th>')
+                .append('<th></th>')
+                .append('<th>Status</th>')
+                .append('<th>Online</th>');
             
-            if (obj.stream) {
-                div.append('<p class="stream">Game: ' + obj.stream + '</p>');
-            }
+            $table
+                .empty()
+                .addClass('table')
+                .append($thead);
+ 
+            data.forEach(function (c) {
                 
-            outputEl.append(div);
-                
-        } else if (data) {
+                if (c && !c.channel.error) {
+                    var $tr       = $('<tr></tr>'),
+                        $tdImage  = $('<td></td>'),
+                        $tdName   = $('<td></td>'),
+                        $tdStatus = $('<td></td>'),
+                        $tdOnline = $('<td></td>'),
+                        $image    = $('<img />'),
+                        $anchor   = $('<a></a>'),
+                        $pStatus  = $('<p></p>'),
+                        $pGame    = $('<p></p>');
+                    
+                    $image
+                        .addClass('logo')
+                        .attr('src', c.channel.logo)
+                        .attr('alt', c.channel.display_name + 'logo');
+                    $anchor
+                        .attr('href', c.channel.url)
+                        .attr('target', '_blank')
+                        .append(c.channel.display_name);
+                    
+                    $tdImage
+                        .append($image);
+                    
+                    $tdName
+                        .append($anchor);
+                    
+                    $tdStatus
+                        .append(c.channel.status);
+                    
+                    $tdOnline
+                        .addClass('online')
+                    
+                    if (c.stream.stream) {
+                        $pGame
+                            .addClass('now-streaming')
+                            .append(c.stream.stream.game);
+                        $tdStatus
+                            .append($pGame);
+                        $tdOnline
+                            .append('<span class="greendot"></span>');
+                    } else {
+                        $tdOnline
+                            .append('<span class="reddot"></span>');
+                    }
+
+                    $tr
+                        .append($tdImage)
+                        .append($tdName)
+                        .append($tdStatus)
+                        .append($tdOnline);
+                    
+                    $tbody.append($tr);
+                    $table.append($tbody);
             
-            mess = [
-                '<div class="user" style="background-color:#ff6961;">',
-                '<p style="text-align: center;">' + data.channel.message + '</p>', '</div>'
-            ].join('');
-            
-            outputEl.append(mess);
+                } else {
+                    
+                    var $tr       = $('<tr></tr>'),
+                        $tdStatus = $('<td></td>');
+                        
+                    $tdStatus
+                        .attr('colspan', 4)
+                        .addClass('online')
+                        .append(c.channel.message);
+                    $tr
+                        .append($tdStatus);
+                    
+                    $tbody.append($tr);
+                    $table.append($tbody);
+                }
+            });
         }
-        
-                
-    }
+    };
     
+    twitchModule.init();
     
-    // ============================ EVENT HANDLERS ============================
-
-    // ALL click handler
-    $('.clickAll').click(function (event) {
-        
-        var status = 'all';
-        
-        // empty the target element
-        $('.twitch').empty();
-        
-        users.forEach(function getChannel(user) {
-            $.getJSON(route.channel + user)
-                .then(getStream)
-                .then(function (data) {return filterObject(data, status); })
-                .then(updateDom);
-        });
-        
-    });
-
-    // ONLINE click handler
-    $('.clickOnline').click(function (event) {
-        
-        var status = 'Online';
-
-        // empty the target element
-        $('.twitch').empty();
-
-        users.forEach(function getChannel(user) {
-            $.getJSON(route.channel + user)
-                .then(getStream)
-                .then(function (data) {return filterObject(data, status); })
-                .then(updateDom);
-        });
-    });
-
-    // OFFLINE click handler
-    $('.clickOffline').click(function (event) {
-        
-        var status = 'Offline';
-
-        // empty the target element
-        $('.twitch').empty();
-
-        users.forEach(function getChannel(user) {
-            $.getJSON(route.channel + user)
-                .then(getStream)
-                .then(function (data) {return filterObject(data); })
-                .then(updateDom);
-        });
-    });
-
-
 });
