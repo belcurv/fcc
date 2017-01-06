0430d51a321724a97eaa3ca1bdadeb4cd4efe606
Jay Schwane <jrschwane@uwalumni.com>
2017-01-05 23:18:46 -0600

    Finish wherefore art thou

--- intermedia_algos/wherefore_art_thou.js
+++ intermedia_algos/wherefore_art_thou.js
@@ -26,51 +26,53 @@
 
 */
 
-
 function whatIsInAName(collection, source) {
     // What's in a name?
     var arr = [];
     // Only change code below this line
     
+    console.log('source:  ', source);
+
     var sourceKeys = Object.keys(source);
     
-    collection.forEach(function (obj) {
-//        console.log(obj);
-        sourceKeys.forEach(function (key) {
-            if (obj.hasOwnProperty(key) && obj[key] === source[key]) {
-                console.log(obj);
-            }
-        });
+    arr = collection.filter(function (elem) {
         
+        console.log('element: ', elem);
+        
+        // .every() returns true if EVERY element passes
+        return sourceKeys.every(function (key) {
+            
+            // return truthyness of expression
+            return elem.hasOwnProperty(key) && elem[key] === source[key];
+        });
     });
-    
-    
+        
     // Only change code above this line
     return arr;
 }
 
-console.log('should return \n [{ a: 1, b: 2 }, { a: 1, b: 2, c: 2 }]');
+//console.log('should return \n [{ a: 1, b: 2 }, { a: 1, b: 2, c: 2 }]');
 console.log(
     whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 })
     // should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }]
 );
 
 // passes
-//console.log(
-//    whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
-//    // should return [{ first: "Tybalt", last: "Capulet" }]
-//);
+console.log(
+    whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
+    // should return [{ first: "Tybalt", last: "Capulet" }]
+);
 
 // passes
-//console.log(
-//    whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 })
-//    // should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
-//);
+console.log(
+    whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 })
+    // should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
+);
 
 
 // fails
-//console.log(
-//    whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 })
-//    // should return [{ "a": 1, "b": 2, "c": 2 }]
-//);
+console.log(
+    whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 })
+    // should return [{ "a": 1, "b": 2, "c": 2 }]
+);
 
