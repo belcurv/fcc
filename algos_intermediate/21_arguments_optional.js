6668012af495ab32c8e40df8e3ea4ade54d5af28
Jay Schwane <jrschwane@uwalumni.com>
2017-01-14 08:44:30 -0600

    Comment cleanup

--- algos_intermediate/21_arguments_optional.js
+++ algos_intermediate/21_arguments_optional.js
@@ -1,18 +1,21 @@
 /* jshint esversion:6 */
 /* globals console */
 
-/* Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
-
-For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
-
-Calling this returned function with a single argument will then return the sum:
-
-var sumTwoAnd = addTogether(2);
-
-sumTwoAnd(3) returns 5.
-
-If either argument isn't a valid number, return undefined.
-
+/* Create a function that sums two arguments together. If only one argument
+   is provided, then return a function that expects one argument and
+   returns the sum.
+   
+   For example, addTogether(2, 3) should return 5, and addTogether(2) should
+   return a function.
+   
+   Calling this returned function with a single argument will then return the
+   sum:
+   
+      var sumTwoAnd = addTogether(2);
+      
+      sumTwoAnd(3) // returns 5.
+      
+      If either argument isn't a valid number, return undefined.
 */
 
 function addTogether() {
@@ -30,7 +33,7 @@ function addTogether() {
         }
     }
 
-    // if less then 2 args, return a function, otherwise add args
+    // less then 2 args, return a function; otherwise sum args
     if (args.length < 2) {
         return (b) => (typeof b === 'number') ? (args[0] + b) : undefined;
     } else {
