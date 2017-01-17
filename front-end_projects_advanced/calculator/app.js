6c28d573ed2ad01fb0f6f382c94525e6452e8010
Jay Schwane <jrschwane@uwalumni.com>
2017-01-17 00:39:54 -0600

    Refactor calculator to jQuery. So much simpler

--- front-end_projects_advanced/calculator/app.js
+++ front-end_projects_advanced/calculator/app.js
@@ -1,169 +1,161 @@
 /* jshint esversion:6 */
-/* globals angular, console, document */
+/* globals $, console, document */
 
-(function () {
+
+$(document).ready(function () {
+
     'use strict';
-    
-    angular
-        .module('calcApp', [])
-    
-        .controller('calcController', [function () {
-            
-            var vm = this;
-            
-            vm.hello = 'Hello world!';
-            
-        }])
-    
-        .directive('calculator', function () {
-        
-            var template = [
-                '<div class="calc-body">',
-                  '<div id="display" class="calc-display></div>',
-                  
-                  '<div id="btn-clear" data-value="c" class="btn-clear"> C </div>',
-                  
-                  '<div id="buttons" class="btn-container">',
-                    '<div data-value="7" class="btn calc-numbers"> 7 </div>',
-                    '<div data-value="8" class="btn calc-numbers"> 8 </div>',
-                    '<div data-value="9" class="btn calc-numbers"> 9 </div>',
-                    '<div data-value="/" class="btn calc-functions"> &divide; </div>',
-                    '<div data-value="4" class="btn calc-numbers"> 4 </div>',
-                    '<div data-value="5" class="btn calc-numbers"> 5 </div>',
-                    '<div data-value="6" class="btn calc-numbers"> 6 </div>',
-                    '<div data-value="*" class="btn calc-functions"> x </div>',
-                    '<div data-value="1" class="btn calc-numbers"> 1 </div>',
-                    '<div data-value="2" class="btn calc-numbers"> 2 </div>',
-                    '<div data-value="3" class="btn calc-numbers"> 3 </div>',
-                    '<div data-value="-" class="btn calc-functions"> - </div>',
-                    '<div data-value="." class="btn calc-numbers"> . </div>',
-                    '<div data-value="0" class="btn calc-numbers"> 0 </div>',
-                    '<div data-value="=" class="btn calc-functions"> = </div>',
-                    '<div data-value="+" class="btn calc-functions"> + </div>',
-                  '</div>',
-                
-                '</div>'
-            ].join('');
-        
-            function link(scope, elem, attrs) {
-                
-                // init variables
-                var regA = null,
-                    regB = null,
-                    opr,
-                    screen = '0';
-                    
-                
-                // cache DOM elements
-                var display = angular.element(document.getElementById('display')),
-                    btnClear = angular.element(document.getElementById('btn-clear')),
-                    btnContainer = angular.element(document.getElementById('buttons'));
-                
-                
-                // bind events
-                btnContainer.on('click', clickHandler);
-                btnClear.on('click', clear);
-                
-                
-                // handle clicks to main button group
-                function clickHandler(evt) {
-                    
-                    // ignore clicks on parent element
-                    if (evt.target !== evt.currentTarget) {
-                                                
-                        var btnValue = evt.target.dataset.value;
-                                                
-                        if (/[0-9]/.test(btnValue)) {
-                            handleNumbers(btnValue);
-                        } else {
-                            handleMath(btnValue);
-                        }
-                        
-                        render(btnValue);
-                    }
-                    
-                    evt.stopPropagation();
-                }
-                
-                
-                // handle clicks to 'clear' button
-                function clear(evt) {
-                    regA = 0;
-                    regB = 0;
-                    screen = '0';
-                    
-                    render();
-                    
-                    evt.stopPropagation();
-                }
 
-                
-                function render(value) {
-                    
-                    console.log(regA);
-                    
-                    if (regA) {
-                        screen = regA.toString();
-                    } else {
-                        screen = '0';
-                    }
-                    
-                    display.html(screen);
-                }
-                
-                
-                function handleNumbers(value) {
-                    
-                    console.log(typeof value);
-                    
-                    // store vals in registers
-                    regA += value;
-                    console.log('Number! ' + value);
-                }
-                
-                
-                function handleMath(operator) {
-                    
-                    // operate on values in registers
-                    // when operator is clicked, send screen values to
-                    // register A or B
-                    console.log('Operator! ' + operator);
-                }
-                
-                render();
-                
+    var regA,
+        regB,
+        opr,
+        output = '',
+        
+        // cache DOM elements
+        $calculator = $('#calculator'),
+        $display    = $calculator.find('.calc-display'),
+        $clearBtn   = $calculator.find('.btn-clear'),
+        $buttons    = $calculator.find('#buttons');
+
+    
+    // bind main button click events
+    $buttons.on('click', function (event) {
+        
+        var val = event.target.dataset.value;
+        
+        if (event.target !== event.currentTarget) {
+
+            if (/[0-9]/.test(val)) {
+                onClickNum( parseFloat(val) );
+            } else if (val === '.') {
+                onClickNum(val);
+            } else if (val === 'eval') {
+                onClickEval();
+            } else {
+                onClickOper(val);
             }
-        
-            return {
-                replace: true,
-                restrict: 'EA',
-                scope: {},
-                link: link,
-                template: template
-            };
+        }
 
-        })
+        event.stopPropagation();
+
+    });
     
-        // Standard footer directive
-        .directive('belcurvFooter', function () {
+    
+    // bind clear button click events
+    $clearBtn.on('click', function () {
+        console.log('Clear!');
+        regA = undefined;
+        regB = undefined;
+        opr = undefined;
+        output = '';
         
-            return {
-                restrict: 'AE',
-                scope: {
-                    name: '@',
-                    version: '@'
-                },
-                template: [
-                    '<p class="footer-credit">{{ name }} {{ version }} : : by ',
-                    '<a href="https://github.com/belcurv" target="_blank">belcurv</a> ',
-                    '<a href="https://github.com/belcurv" target="_blank">',
-                    '<svg width="20" height="20" class="github-icon" viewBox="0 0 16 16" version="1.1" aria-hidden="true">',
-                    '<path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"> </path>',
-                    '</svg></a></p>',
-                    '<br />',
-                    '<p class="footer-credit">Source released under the MIT license. Website and documentation licensed under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>.</p>'
-                ].join('')
-            };
+        logState();
+        render();
     });
 
-}());
+
+    // number button click handler
+    function onClickNum(num) {
+
+        // convert numbers to strings
+        if (typeof num === 'number') {
+            num = num.toString();
+        }
+
+        if (opr === undefined && regA === undefined) {
+            regA = num;
+            output += num;
+        } else if (opr === undefined && regA) {
+            regA += num;
+            output += num;
+        } else if (regB === undefined) {
+            regB = num;
+            output += num;
+        } else {
+            regB += num;
+            output += num;
+        }
+        
+        logState();
+        render();
+    }
+
+
+    // operator button click handler
+    function onClickOper(oper) {
+        opr = oper;
+        output += oper;
+        logState();
+        render();
+    }
+
+    
+    // eval button click handler
+    function onClickEval() {
+
+        var registerA = parseFloat(regA),
+            registerB = parseFloat(regB),
+            decimals;
+
+        switch (opr) {
+            case '*':
+                output = registerA * registerB;
+                break;
+            case '/':
+                output = registerA / registerB;
+                break;
+            case '+':
+                output = registerA + registerB;
+                break;
+            case '-':
+                output = registerA - registerB;
+                break;
+        }
+        
+        // format decimals
+        if (countDecimals(output) > 10) {
+            output = output.toFixed(10);
+        } else {
+            output = output.toFixed(countDecimals(output));
+        }
+        
+        logState();
+        render();
+        
+        regA = output;
+        regB = undefined;
+        opr = undefined;
+    }
+    
+    
+    // log the machine state
+    function logState() {
+        console.log('RegA: ' + regA, '| Opr: ' + opr, '| RegB: ' + regB);
+    }
+    
+    
+    // render DOM
+    function render() {
+
+        if (output === undefined || output === '') {
+            $display.html(0);
+        } else {
+            $display.html(output);
+        }        
+    }
+    
+    
+    // count number of decimals in output
+    function countDecimals(num) {
+        if (Math.floor(num) === num) {
+            return 0;
+        }
+        
+        return num.toString().split(".")[1].length || 0;
+    }
+    
+
+    // render on 1st hit
+    render();
+
+});
